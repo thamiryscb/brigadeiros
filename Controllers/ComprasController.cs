7027e3e5eaaba1d09301eafeee9b211be83b075f
Thamirys <thamirys.barbosa@escolar.ifrn.edu.br>
2023-11-07 10:26:22 -0300

    Thamirys - Compras 02

--- Controllers/ComprasController.cs
+++ Controllers/ComprasController.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using apiBrigadeiro.Context;
 using apiBrigadeiro.Model;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
