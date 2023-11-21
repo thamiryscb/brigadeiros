6d8dfd860d6be59d1b626b0b56022518dacbbbe9
Jainyy <ribeiro.eloise@escolar.ifn.edu.br>
2023-11-21 11:19:43 -0300

    TESTE

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
 using apiBrigadeiro.Context;
 using apiBrigadeiro.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
 
 namespace apiBrigadeiro.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    
     public class ClienteController : ControllerBase
     {
         private readonly ILogger<ClienteController> _logger;
